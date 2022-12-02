7abc6d45015e8fb7362b09aaccd9d4991d22ae24
sju3358 <tjdfkr011@naver.com>
2022-12-02 14:36:29 +0900

    WelcomePage Controller 간단구현

--- hello-spring/src/main/java/hello/hellospring/controller/helloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/helloController.java
@@ -1,8 +1,10 @@
 package hello.hellospring.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class helloController {
 
     @GetMapping("hello")//웹에서 hello 메세지를 받으면 아래 함수 실행
